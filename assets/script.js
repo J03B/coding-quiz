a866ffb40ad1ebd9084599841c316b15a3e9f82d
J03B <byucrazyfan@gmail.com>
2022-12-12 19:22:16 -0600

    Added README and all other basic barebone pseudocode file structure

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// Setting document variables for text editing sections
+
+
+
+// Setting initial home screen to remove placeholders
+
+
+
+// Once the quiz starts, we can change all texts
+// First set an array for each question, its choices, and answers
+
+
+
+// START OF QUIZ
+// Run through a loop of the questions
+
+
+
+
+
+// END OF QUIZ - SHOW HIGH SCORES
