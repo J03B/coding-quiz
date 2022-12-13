07fd2db0d4388bbae54587af094c4725c951790b
J03B <byucrazyfan@gmail.com>
2022-12-12 19:54:09 -0600

    Added class and interactive styles to css and base variables to js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,21 @@
 // Setting document variables for text editing sections
+var timeLeft = document.querySelector(".time");
+var question = document.querySelector(".question");
+var subtext = document.querySelector(".subtext");
+var result = document.querySelector(".result");
+var choices = ["c1","c2","c3","c4"]
 
-
-
+import { allQuestions } from "./questions";
 // Setting initial home screen to remove placeholders
 
 
 
 // Once the quiz starts, we can change all texts
 // First set an array for each question, its choices, and answers
-
-
+import { allQuestions } from './questions.js';
+import { allChoices } from "./questions";
+import { allResults } from "./questions";
+var choiceOrder = ["A","B","C","D"];
 
 // START OF QUIZ
 // Run through a loop of the questions
